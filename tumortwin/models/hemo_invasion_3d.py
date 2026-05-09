1f41f246e40b863b5f8990aca8b82ddbbd10f4ff
Kate Radionova <83696885+ferd244@users.noreply.github.com>
2026-05-09 20:01:33 +0300

    Update hemo_invasion_3d.py

--- tumortwin/models/hemo_invasion_3d.py
+++ tumortwin/models/hemo_invasion_3d.py
@@ -354,8 +354,13 @@ class HemoInvasion3D(PDESystemModel3D):
 
     def callback_step(self, t, u, dt):
         if self.progress_bar is not None:
-            self.progress_bar.update(dt.item())
-
+            try:
+                new_n = int(t.item() + dt.item())
+                delta = new_n - int(self.progress_bar.n)
+                if delta > 0:
+                    self.progress_bar.update(delta)
+            except (TypeError, AttributeError, ValueError):
+                pass
         self.validate_state_shape(u, allow_batch=False)
 
         tissue = self.comp_mask.to(device=u.device, dtype=u.dtype)
