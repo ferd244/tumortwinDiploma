827184ebf854b8a743dd77d59258d9b5cfd3f28c
Kate Radionova <83696885+ferd244@users.noreply.github.com>
2026-05-13 21:53:12 +0300

    Update __init__.py

--- tumortwin/solvers/__init__.py
+++ tumortwin/solvers/__init__.py
@@ -1,2 +1,6 @@
 # flake8: noqa: F401
-from .torch_solver import TorchDiffEqSolver, TorchDiffEqSolverOptions
+from .torch_solver import (
+    ADAPTIVE_TORCHDIFFEQ_METHODS,
+    TorchDiffEqSolver,
+    TorchDiffEqSolverOptions,
+)
